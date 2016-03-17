2d83f529e967c5136ae955d52aab0a04a448470f
Administrator <richard1861983@gmail.com>
2016-03-17 16:46:08 +0800

    JavaTest关于打印的测试程序

---
+++ JavaTest/src/main/java/PrintTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/3/17.
+ */
+public class PrintTest {
+    public static void main (String[] args ) {
+        System.out.println("Test!!!");
+    }
+}
---
+++ JavaTest/src/main/java/abc.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016/3/17.
+ */
+public class abc {
+}
